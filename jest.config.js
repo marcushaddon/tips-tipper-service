0561f225d1ecabf41af5f42b752fca073dae3c19
Marcus Haddon <marcus.haddon@nike.com>
2020-03-26 09:27:03 -0700

    restore jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "roots": [
-      "<rootDir>/src"
-    ],
-    testMatch: [
-      "**/?(*.)+(spec|test).+(ts|tsx|js)"
-    ],
-    "transform": {
-      "^.+\\.(ts|tsx)$": "ts-jest"
-    },
-    "coverageReporters": [
-      "lcov",
-      "text",
-      "text-summary",
-      "json",
-      "json-summary"
-    ]
-  }
+  "roots": [
+    "<rootDir>/src"
+  ],
+  testMatch: [
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "coverageReporters": [
+    "lcov",
+    "text",
+    "text-summary",
+    "json",
+    "json-summary"
+  ]
+}
