71487997cfb8a12a23891393e59952af7915dfb8
Marcus Haddon <marcus.haddon@nike.com>
2020-03-26 09:05:26 -0700

    create dynamo table and role

--- src/main/model/TipsUser.ts
+++ src/main/model/TipsUser.ts
@@ -1,5 +1,10 @@
 type TipsUser = {
-
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    venmo?: string;
+    paypal?: string;
+    preferredMethod?: string;
 };
 
 export default TipsUser;
