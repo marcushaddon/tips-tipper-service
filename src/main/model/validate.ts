77e3c4e579ed10782ea8859797586ca798d33aeb
Marcus Haddon <marcus.haddon@nike.com>
2020-04-01 17:26:57 -0700

    create schema build script (for now)

--- src/main/model/validate.ts
+++ src/main/model/validate.ts
@@ -1,32 +1,21 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import * as TJS from 'typescript-json-schema';
 import { validate as vdt, JSONSchema7, ValidationResult } from 'json-schema';
 
-// optionally pass argument to schema generator
-const settings: TJS.PartialArgs = {
-    required: true
-};
+const schemas: { [ key: string ]: any } = {};
+fs
+    .readdirSync(__dirname)
+    .filter(f => f.indexOf('schema.json') > -1)
+    .forEach(f => {
+        const name = f.replace('.schema.json', '');
+        schemas[name] = require(path.join(__dirname, f));
+    })
 
-// optionally pass ts compiler options
-const compilerOptions: TJS.CompilerOptions = {
-    strictNullChecks: true
-}
-
-const modelFiles = fs.readdirSync(__dirname)
-    .filter(f => f !== 'schema.ts')
-    .map(f => path.join(__dirname, f));
-
-
-const program = TJS.getProgramFromFiles(modelFiles, compilerOptions);
-const generator = TJS.buildGenerator(program, settings);
-
+console.log(schemas);
 // TODO: figure out why this is validating everything
-const validate = (obj: any, symbol: string): ValidationResult => {
-    const schema = generator!.getSchemaForSymbol(symbol);
-    const res = vdt(obj, schema as JSONSchema7);
 
-    return res;
-};
+const validate = (instance: any, schema: string) => {
+    return vdt(instance, schemas[schema]);
+}
 
 export default validate;
